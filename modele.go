5c4e823914659ae4f86e2ce6136d2ca8b652cc1c
Mendyk <lukarzmen@gmail.com>
2018-01-10 23:08:09 +0100

    jak wziac date utworzenia

--- modele.go
+++ modele.go
@@ -6,6 +6,10 @@ type Zadanie struct {
 	SkroconyURL string `json:"shortUrl"`
 }
 
+//jak wziąć datę utworzenia zadania
+//http://help.trello.com/article/759-getting-the-time-a-card-or-board-was-created
+//new Date(1000*parseInt(idBoard.substring(0,8),16));
+
 type ListaZadan struct {
 	Zadania     []Zadanie `json:"tasks,omitempty"`
 	LiczbaZadan int       `json:"tasks_count"`
